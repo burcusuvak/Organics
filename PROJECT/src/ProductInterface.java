58842bd4f910e38ba6b9843c03af4cadfde52796
Gulzada IISAEVA <iisaevagulzada@gmail.com>
2018-04-24 16:10:26 +0300

    Delete ProductInterface.java

--- PROJECT/src/ProductInterface.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface ProductInterface {
-    public  String getName();
-    public  double getPrice();
-    public ArrayList<String> getContent();
-    public void setName(String name);
-    public void setPrice(double price);
-}
